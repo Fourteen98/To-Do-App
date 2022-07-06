4ac9095594cd74c537d88d06f986cb99ab6b92da
Fourteen98 <emuhiydeen6@gmail.com>
2022-07-06 23:58:12 +0000

    create todo list structure

--- src/index.js
+++ src/index.js
@@ -1,19 +1,62 @@
 import _ from 'lodash';
-import printMe from './print.js';
+import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+const inputTodo = document.getElementById('input-todo');
+const addTodo = document.querySelector('.add-todo');
+const error = document.querySelector('.error-msg');
+const mainListCard = document.querySelector('.main-list-card');
+const reload = document.querySelector('.reload');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+let todoTask = [
+  {
+    description: 'Typing',
+    isCompleted: false,
+    index: 3,
+  },
+  {
+    description: 'Listening',
+    isCompleted: false,
+    index: 2,
+  },
+  {
+    description: 'Take Nap',
+    isCompleted: false,
+    index: 1,
+  },
+  {
+    description: 'Gaming',
+    isCompleted: false,
+    index: 0,
+  },
+];
 
-  btn.innerHTML = 'Click me and check the console';
-  btn.onclick = printMe;
+const createTask = (task) => {
+  const todoCard = document.createElement('div');
+  todoCard.classList.add('todo');
 
-  element.appendChild(btn);
+  const divide = document.createElement('div');
+  divide.classList.add('divide');
 
-  return element;
-}
+  const inputType = document.createElement('input');
+  inputType.setAttribute('type', 'checkbox');
+  inputType.id = task.index;
+  divide.appendChild(inputType);
 
-document.body.appendChild(component());
+  const label = document.createElement('label');
+  label.setAttribute('for', task.index);
+  label.innerText = ` ${task.description}`;
+  divide.appendChild(label);
+
+  const ellipsis = document.createElement('i');
+  ellipsis.classList.add('fas');
+  ellipsis.classList.add('fa-ellipsis-v');
+
+  todoCard.appendChild(divide);
+  todoCard.appendChild(ellipsis);
+
+  mainListCard.appendChild(todoCard);
+};
+
+todoTask.forEach((task) => {
+  createTask(task)
+});
