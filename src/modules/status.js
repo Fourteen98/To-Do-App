c49db1bb222480e3fe65fcf01b11c74821d16e8c
Fourteen98 <emuhiydeen6@gmail.com>
2022-07-11 19:30:24 +0000

    add strikethrough when task is checked

--- src/modules/status.js
+++ src/modules/status.js
@@ -4,19 +4,28 @@ const localS = new LocalStorage();
 
 class Status {
     updateTaskCompletedStatus = (index, status) => {
-      if (index >= localS.getLocalStorage().length) {
-        index -= 1;
-      }
-      const todo = localS.getLocalStorage();
-      todo[index].completed = status;
-      localStorage.setItem('todo', JSON.stringify(todo));
+        if (index >= localS.getLocalStorage().length) {
+            index -= 1;
+        }
+        const todo = localS.getLocalStorage();
+        todo[index].completed = status;
+        localStorage.setItem('todo', JSON.stringify(todo));
     }
 
     clearTaskCompleted = () => {
-      const todo = localS.getLocalStorage();
-      const newTodo = todo.filter((task) => task.completed === 0);
-      localStorage.setItem('todo', JSON.stringify(newTodo));
+        const todo = localS.getLocalStorage();
+        const newTodo = todo.filter((task) => task.completed === 0);
+        localStorage.setItem('todo', JSON.stringify(newTodo));
     }
+
+    // a function that strikesthrough text when task is completed
+    strikethroughText = (index) => {
+        const todo = localS.getLocalStorage();
+        if (todo[index].completed === 1) {
+            document.querySelector(`.${index}`).classList.add('strikethrough');
+        }
+    }
+
 }
 
 export default Status;
